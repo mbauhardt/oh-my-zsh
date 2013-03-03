eae163db866f64deba94cbd153e21445f8cb9099
Marko Bauhardt <marko.bauhardt@googlemail.com>
2013-03-03 20:36:34 +0100

    [#11] add update and uninstalller to the antw plugin

--- plugins/antw/antw.plugin.zsh
+++ plugins/antw/antw.plugin.zsh
@@ -1,11 +1,21 @@
-
 function antw {
   if [[ -z $ANTW_HOME ]]; then
     ANTW_HOME=$HOME/.antw/install
   fi
-
   $ANTW_HOME/bin/antw $@
 }
-
 compdef _ant antw
 
+function antw-update {
+  if [[ -z $ANTW_HOME ]]; then
+    ANTW_HOME=$HOME/.antw/install
+  fi
+  $ANTW_HOME/bin/antw-update
+}
+
+function antw-uninstall {
+  if [[ -z $ANTW_HOME ]]; then
+    ANTW_HOME=$HOME/.antw/install
+  fi
+  $ANTW_HOME/bin/antw-uninstall
+}
